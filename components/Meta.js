c021e0646a51d1afaff044bf4a25e62ec309cdc5
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-09 15:30:48 -0700

    Removed default next stuff

---
+++ components/Meta.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const Meta = () => {
+    return (
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <title>DEMON LABS</title>
+            <meta name="description" content="
+            We're creating the new world. 
+            " />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+    )
+}
+
+export default Meta
