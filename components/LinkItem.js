9908afdeee4bffd34d96ad6de6010a1ee2119b2f
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-10 14:27:29 -0700

    Finished everything I think

--- components/LinkItem.js
+++ components/LinkItem.js
@@ -1,15 +1,15 @@
-
+import { useState } from 'react'
 
 import styles from '../styles/Home.module.css'
 
 const LinkItem = ({ title, link, linkName }) => {
 
-
+    const [underlined, setUnderlined] = useState(false);
 
     return (
         <div className={styles.linkItem}>
             <p className={styles.linkItemTitle}>{title}</p>
-            <a rel="noopener noreferrer" target="_blank" className={styles.linkItemLink} href={link}>{linkName}</a>
+            <a style={underlined ? {textDecoration: 'underline'} : {}} onMouseEnter={() => setUnderlined(true)} onMouseOut={() => setUnderlined(false)} rel="noopener noreferrer" target="_blank" className={styles.linkItemLink} href={link}>{linkName}</a>
         </div>
     )
 }
