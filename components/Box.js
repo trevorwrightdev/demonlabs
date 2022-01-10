9f57b00cd6415a83922a9e5a3267d0fb8c80dadb
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-10 10:48:31 -0700

    Added a cool animatino

--- components/Box.js
+++ components/Box.js
@@ -3,34 +3,40 @@ import { motion } from 'framer-motion'
 import styles from '../styles/Home.module.css'
 
 const Box = () => {
+
+    const shakeVariants = {
+        down: {
+            y: '3%',
+        },
+        up: {
+            y: '-3%',
+            transition: {
+                yoyo: Infinity,
+                duration: 0.1,
+            }
+        }
+    }
+
+
     return (
         <div className={styles.box}>
-            <motion.div className={styles.motionDiv} initial='hidden' animate='visible' variants={{
+            <motion.span initial='hidden' animate='visible' variants={{
                 hidden: {
                     scale: .8,
                     opacity: 0,
                 },
-
                 visible: {
                     scale: 1,
                     opacity: 1,
                     transition: {
-                        type: 'spring',
-                        delay: 0.4,
-                        stiffness: 60,
-                    },
+                        delay: .4,
+                        duration: 1
+                    }
                 }
             }}>
-                <h1 className={styles.boxHeader}>DEMON LABS</h1>
-            </motion.div>
-            <motion.div initial='hidden' animate='visible' variants={{
-                hidden: {
-                    opacity: 0,
-                    
-                }
-            }}>
-                <h2>Loading worlds...</h2>
-            </motion.div>
+                <motion.h1 initial='down' animate='up' variants={shakeVariants}>DEMON LABS</motion.h1>
+                <p className={styles.desc}>Demon Labs develops stylish and polished games for NFT projects.</p>
+            </motion.span>
         </div>
     )
 }
