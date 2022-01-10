17802788b3d338b4e9eef586c62e512af22bc1b8
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-10 16:23:21 -0700

    Added logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,11 +1,12 @@
-import logoImage from '../public/images/demonlabslogo.png'
-
+import logoImage from '../public/images/demonlabslogoscaled.png'
 import Image from 'next/image'
+
+import styles from '../styles/Home.module.css'
 
 const Logo = () => {
     return (
-        <div>
-            <Image src={logoImage}/>
+        <div className={styles.logo}>
+            <Image className={styles.logo} src={logoImage} width={140} height={145} quality={100}/>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Meta from '../components/Meta'
 import Box from '../components/Box'
 import Worlds from '../components/Worlds'
 import LinkItem from '../components/LinkItem'
+import Logo from '../components/Logo'
 
 import { motion } from 'framer-motion'
 
@@ -30,6 +31,7 @@ export default function Home() {
       <Meta />
       <div className={styles.container}>
         <Box>
+            <Logo />
             <motion.h1 initial='down' animate='up' variants={shakeVariant}><span className={styles.small}>name: </span>DEMON LABS</motion.h1>
             <p className={styles.desc}>desc: Demon Labs develops stylish and polished games for NFT projects.</p>
             <Worlds />
