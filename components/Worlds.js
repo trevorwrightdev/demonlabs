a979e0bfb3803434b5a7b509f343a07946901efb
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-10 12:11:04 -0700

    Removed translation thing, it was giving me an error.

--- components/Worlds.js
+++ components/Worlds.js
@@ -6,12 +6,16 @@ const Worlds = () => {
 
     const [underlined, setUnderlined] = useState(false);
 
-
-
     return (
         <div className={styles.worlds}>
-            <p>worlds:</p>
-            <a className={styles.neonCity} rel="noopener noreferrer" target="_blank" href='https://supercyborgs.io' style={underlined ? {textDecoration: 'underline'} : {}} onMouseEnter={() => setUnderlined(true)} onMouseOut={() => setUnderlined(false)}>Neon City</a>
+            <p>worlds:</p> 
+            <a 
+                className={styles.neonCity} 
+                rel="noopener noreferrer" target="_blank" href='https://supercyborgs.io' 
+                style={underlined ? {textDecoration: 'underline'} : {}} 
+                onMouseEnter={() => setUnderlined(true)} 
+                onMouseOut={() => setUnderlined(false)}>Neon City
+            </a>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,17 @@ import Meta from '../components/Meta'
 import Box from '../components/Box'
 import styles from '../styles/Home.module.css'
 
+
+// ? <html lang='en' className='notranslate' translate='no'>
+
 export default function Home() {
   return (
-    <html lang='en' className='notranslate' translate='no'>
-    <Meta />
-    <div className={styles.container}>
-      <Box />
-    </div>
-    </html>
+    <>
+      <Meta />
+      <div className={styles.container}>
+        <Box />
+      </div>
+    </>
   )
 }
 
