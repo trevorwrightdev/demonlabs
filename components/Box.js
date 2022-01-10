57cba77ab37a71a53fd12092d6fe2e76285e482a
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-09 18:53:27 -0700

    Added demon labs header

--- components/Box.js
+++ components/Box.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 const Box = () => {
     return (
         <div className={styles.box}>
-            
+            <h1 className={styles.boxHeader}>DEMON LABS</h1>
         </div>
     )
 }
