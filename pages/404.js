d90936d9ceeac3a592592f25732d21eafed15a5e
trevorwrightdev <trevor.wright.710@gmail.com>
2022-01-12 12:03:50 -0700

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Meta from '../components/Meta'
+
+import errorStyles from '../styles/Error.module.css'
+
+export default function Custom404() {
+    return (
+    <>
+    <Meta />
+    <div className={errorStyles.grid}>
+        <h1 className={errorStyles.errorCode}>404</h1>
+    </div>
+    </>
+    )
+}
